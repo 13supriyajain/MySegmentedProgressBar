2822d394a208a3863c23bb7e29e45504f11e325c
13supriyajain <13supriya.jain@gmail.com>
2018-06-06 13:10:37 -0700

    Initial Commit

---
+++ MainActivity.java
@@ -0,0 +1,93 @@
+package com.supjain.mysegmentedprogressbar;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView mImageView = (ImageView) findViewById(R.id.image_view);
+
+        Bitmap bitmap = Bitmap.createBitmap(
+            getScreenWidth(), // Width
+                300, // Height
+                Bitmap.Config.ARGB_8888 // Config
+        );
+
+        Canvas canvas = new Canvas(bitmap);
+
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLUE);
+
+        Paint textPaint = new Paint();
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setColor(Color.WHITE);
+        textPaint.setTextSize(50);
+
+        int segmentCount = 7;
+        int filledSegmentCount = 4;
+        int gap = 5;
+
+        int marginFromTop = 5;
+        int progressBarHeight = 50;
+
+        int marginFromLeft = 0;
+        int progressBarSegmentWidth = (int)(getScreenWidth() / segmentCount) - gap;
+        int marginFromRight = marginFromLeft + progressBarSegmentWidth;
+
+        for(int i = 0; i < filledSegmentCount; i++)
+        {
+            drawRectangle(canvas, marginFromLeft, marginFromTop, marginFromRight, progressBarHeight, paint);
+            canvas.drawText(String.valueOf(i+1), (marginFromLeft + marginFromRight - gap - gap - gap)/2, progressBarHeight - gap, textPaint);
+            marginFromLeft = marginFromLeft + progressBarSegmentWidth + gap;
+            marginFromRight = marginFromLeft + progressBarSegmentWidth;
+        }
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.BLACK);
+        textPaint.setColor(Color.BLACK);
+        for(int i = filledSegmentCount; i < segmentCount; i++)
+        {
+            drawRectangle(canvas, marginFromLeft, marginFromTop, marginFromRight, progressBarHeight, paint);
+            canvas.drawText(String.valueOf(i+1), (marginFromLeft + marginFromRight - gap - gap - gap)/2, progressBarHeight - gap, textPaint);
+            marginFromLeft = marginFromLeft + progressBarSegmentWidth + gap;
+            marginFromRight = marginFromLeft + progressBarSegmentWidth;
+        }
+
+        mImageView.setImageBitmap(bitmap);
+    }
+
+
+    public void drawRectangle(Canvas canvas, int marginFromLeft, int marginFromTop,
+                              int marginFromRight, int progressBarHeight, Paint paint) {
+        Rect rectangle = new Rect(
+                marginFromLeft, // Left
+                marginFromTop, // Top
+                marginFromRight, // Right
+                progressBarHeight // Bottom
+        );
+
+        canvas.drawRect(rectangle,paint);
+    }
+
+    public int getScreenWidth() {
+//        DisplayMetrics displayMetrics = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+//        return displayMetrics.widthPixels;
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+}
