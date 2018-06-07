b8428cd08b59f69ee515b018ab23f298756280c9
13supriyajain <13supriya.jain@gmail.com>
2018-06-07 14:42:39 -0700

    Added Check Marked Circles to PB

--- MainActivity.java
+++ MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -21,53 +22,63 @@ public class MainActivity extends AppCompatActivity {
 
         Bitmap bitmap = Bitmap.createBitmap(
             getScreenWidth(), // Width
-                100, // Height
+                150, // Height
                 Bitmap.Config.ARGB_8888 // Config
         );
 
         Canvas canvas = new Canvas(bitmap);
 
+        VectorDrawableCompat vectorDrawable = VectorDrawableCompat.create(this.getResources(), R.drawable.ic_check_circle_black_24dp, null);
+
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(getResources().getColor(R.color.light_green));
 
-        Paint textPaint = new Paint();
-        textPaint.setStyle(Paint.Style.FILL);
-        textPaint.setColor(Color.WHITE);
-        textPaint.setTextSize(50);
-
         int segmentCount = 7;
-        int filledSegmentCount = 4;
+        int filledSegmentCount = 3;
+        int circleRadius = 40;
 
-        int marginFromTop = 40;
+        int marginFromTop = 50;
         int progressBarHeight = 60;
 
+        int vectorTop = 0;
+        int vectorBottom = 110;
+
         int marginFromLeft = 0;
-        int progressBarSegmentWidth = (int)(getScreenWidth() / segmentCount);
-        int marginFromRight = marginFromLeft + progressBarSegmentWidth;
-        int circleRadius = 40;
+        int marginFromRight = 0;
+        int progressBarSegmentWidth = (int)(getScreenWidth() / (segmentCount-1)) -15 - (2 * circleRadius) ;
 
         for(int i = 0; i < filledSegmentCount; i++)
         {
-            drawRectangle(canvas, marginFromLeft, marginFromTop, marginFromRight, progressBarHeight, paint);
-            canvas.drawCircle((marginFromRight+marginFromLeft)/2, progressBarHeight-10, circleRadius, paint);
-            canvas.drawText(String.valueOf(i+1),((marginFromRight+marginFromLeft)/2)-15, progressBarHeight+10, textPaint);
-            marginFromLeft = marginFromLeft + progressBarSegmentWidth;
+            //canvas.drawCircle(marginFromRight, progressBarHeight-10, circleRadius, paint);
+            marginFromRight = marginFromRight + 2 * circleRadius;
+            vectorDrawable.setBounds(marginFromLeft, vectorTop, marginFromRight,vectorBottom);
+            vectorDrawable.draw(canvas);
+            marginFromLeft = marginFromRight - 5;
             marginFromRight = marginFromLeft + progressBarSegmentWidth;
+            drawRectangle(canvas, marginFromLeft, marginFromTop, marginFromRight, progressBarHeight, paint);
+            marginFromLeft = marginFromRight - 5;
         }
 
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(getResources().getColor(R.color.light_orange));
-        textPaint.setColor(Color.BLACK);
-        for(int i = filledSegmentCount; i <= segmentCount; i++)
+        Paint circlePaint = new Paint();
+        circlePaint.setStyle(Paint.Style.STROKE);
+        circlePaint.setStrokeWidth(5);
+        circlePaint.setColor(getResources().getColor(R.color.light_green));
+
+        marginFromRight = marginFromRight + circleRadius;
+        canvas.drawCircle(marginFromRight, progressBarHeight-10, circleRadius, circlePaint);
+        marginFromLeft = marginFromRight + circleRadius;
+        marginFromRight = marginFromLeft + progressBarSegmentWidth;
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.GRAY);
+        for(int i = filledSegmentCount; i < segmentCount-1; i++)
         {
-            drawRectangle(canvas, marginFromLeft, marginFromTop, marginFromRight, progressBarHeight, paint);
-            if(i < segmentCount) {
-                canvas.drawCircle((marginFromRight+marginFromLeft)/2, progressBarHeight-10, circleRadius, paint);
-                canvas.drawText(String.valueOf(i+1),((marginFromRight+marginFromLeft)/2)-15, progressBarHeight+10, textPaint);
-                marginFromLeft = marginFromLeft + progressBarSegmentWidth;
-                marginFromRight = marginFromLeft + progressBarSegmentWidth;
-            }
+            drawRectangle(canvas, marginFromLeft, marginFromTop, marginFromRight, progressBarHeight-7, paint);
+            marginFromRight = marginFromRight + circleRadius;
+            canvas.drawCircle(marginFromRight, progressBarHeight-10, circleRadius, paint);
+            marginFromLeft = marginFromRight + circleRadius;
+            marginFromRight = marginFromLeft + progressBarSegmentWidth;
         }
 
         mImageView.setImageBitmap(bitmap);
